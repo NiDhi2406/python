94f3e1b4150db7d1974de3cdeaa62e0ff56115db
root <root@LAPTOP-PVIMTUA2.localdomain>
2022-04-20 18:28:12 +0530

    Extractor

---
+++ phoneandemailextractor.py
@@ -0,0 +1,40 @@
+# get the text off clipboard.
+# find all phone numbers and email addresses in the text.
+# paste them into the clipboard
+# we will be using "pyperclip module" to copy and paste the string
+
+import pyperclip, re
+phoneRegex = re.compile (r'''(
+    (\\d{3} |\\(d{3}\\))?
+    (\\s|-|\\.)?
+    (\\d{3})
+    (\\s|-|\\.)
+    (\\d{4})
+    (\\s* (ext|x|ext.)\\s*(\\d{2, 5}))?
+)''', re.VERBOSE)
+
+# Regex for the email address
+
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-] +
+    @
+    [a-zA-Z0-9.-] +
+    (\\.[a-zA-Z] {2, 4})
+)''', re.VERBOSE)
+
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups [8] != '':
+        phoneNum += ' x' + groups[8]
+        matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+if len(matches) > 0:
+    pyperclip.copy('\\n'.join(matches))
+    print("Copied to clipboard: ")
+    print('\\n'.join(matches))
+else:
+    print("No phone number or email address found.")
